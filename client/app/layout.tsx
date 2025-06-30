5bc88dd07f3b9d9d3c0189bc6aa692fdaf570b10
lakshit68 <lakshitsachdeva25@gmail.com>
2025-06-30 18:05:20 +0530

    update

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,13 +1,11 @@
-import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 import {
   ClerkProvider,
-  SignedOut,
-  SignUp,
   SignedIn,
-  SignUpButton,
-  UserButton,
+  SignedOut,
+  SignUpButton
 } from '@clerk/nextjs';
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 
 const geistSans = Geist({
